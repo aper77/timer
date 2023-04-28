a5158df518c06dc75cf4ba81dd51f44c1662e285
test <test@MacBook-Pro-von-test.local>
2023-04-28 15:32:06 +0400

    Initial commit on main branch

---
+++ src/Timer.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+function Timer() {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  function formatTime(time) {
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor((time - hours * 3600) / 60);
+    let seconds = time - hours * 3600 - minutes * 60;
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  }
+
+  return <div>{formatTime(time)}</div>;
+}
+
+export default Timer;
